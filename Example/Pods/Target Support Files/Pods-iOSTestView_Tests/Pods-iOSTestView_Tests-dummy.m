2c42eff4fa0a11eb6be28445d77d3f53e6c80ac5
Tobias Frantsen <tof@amsiq.com>
2018-11-07 13:49:14 +0100

    create lib via pod

---
+++ Example/Pods/Target Support Files/Pods-iOSTestView_Tests/Pods-iOSTestView_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSTestView_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_iOSTestView_Tests
+@end
---
+++ Example/Pods/Target Support Files/iOSTestView/iOSTestView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_iOSTestView : NSObject
+@end
+@implementation PodsDummy_iOSTestView
+@end
